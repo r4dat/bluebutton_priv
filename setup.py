51c6a14e40d2ab4cdbb5bdc5d7c0f81ddc5798b5
Rob R <RRutherford@khconline.org>
2015-06-25 16:24:00 -0500

    Initial clone of bluebutton.py repo 0.3.0/0.3.0post1.

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""
+BlueButton.py
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='bluebutton',
+    version='0.3.0.post1',
+    packages=setuptools.find_packages(),
+    description='The Blue Button Python Library',
+    author='Taeber Rapczak',
+    author_email='taeber@ufl.edu',
+    url='https://github.com/ctsit/bluebutton.py',
+    license='Apache License, Version 2.0',
+    keywords=[
+        "bb",
+        "blue button",
+        "ccda",
+        "c-cda",
+        "ehr",
+        "emr",
+        "health",
+        "healthcare",
+        "medical",
+        "phr",
+        "record"
+    ],
+    setup_requires=[
+        "nose >= 1.0",
+        "nosexcover >= 1.0.10",
+    ],
+)
