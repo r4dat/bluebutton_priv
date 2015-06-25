65d77c1653e0e5d5e49464bd3a06c7a6a80d4b70
Rob R <RRutherford@khconline.org>
2015-06-25 16:45:34 -0500

    Fix (un)indenting.

--- bluebutton/documents/c32.py
+++ bluebutton/documents/c32.py
@@ -15,21 +15,21 @@ def process(c32):
     return c32
 
 
-  def section(c32, name):
-  """
-   Finds the section of a C32 document
-   Usually we check first for the HITSP section ID and then for the HL7-CCD ID.
-   """
+def section(c32, name):
+    """
+     Finds the section of a C32 document
+     Usually we check first for the HITSP section ID and then for the HL7-CCD ID.
+     """
 
     entries = documents.entries
-    
+
     if 'document' == name:
-      return c32.template('2.16.840.1.113883.3.88.11.32.1')
+        return c32.template('2.16.840.1.113883.3.88.11.32.1')
     if 'allergies' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.102')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.2')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.2')
+
         el.entries = entries
         return el
     if 'demographics' == name:
@@ -37,15 +37,15 @@ def process(c32):
     if 'encounters' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.127')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.3')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.3')
+
         el.entries = entries
         return el
     if 'immunizations' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.117')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.6')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.6')
+
         el.entries = entries
         return el
     if 'results' == name:
@@ -55,31 +55,31 @@ def process(c32):
     if 'medications' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.112')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.8')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.8')
+
         el.entries = entries
         return el
     if 'problems' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.103')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.11')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.11')
+
         el.entries = entries
         return el
     if 'procedures' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.108')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.12')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.12')
+
         el.entries = entries
         return el
     if 'vitals' == name:
         el = c32.template('2.16.840.1.113883.3.88.11.83.119')
         if el.is_empty():
-          el = c32.template('2.16.840.1.113883.10.20.1.16')
-        
+            el = c32.template('2.16.840.1.113883.10.20.1.16')
+
         el.entries = entries
         return el
-    
-    
+
+
     return None
