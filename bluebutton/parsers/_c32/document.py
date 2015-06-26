f1850a392d187f7db482b0a1ad012a6cd179dfb4
Rob R <RRutherford@khconline.org>
2015-06-26 16:07:16 -0500

    Add KHC copyright header.

--- bluebutton/parsers/_c32/document.py
+++ bluebutton/parsers/_c32/document.py
@@ -1,3 +1,9 @@
+###############################################################################
+# Copyright 2015 Kansas Healthcare Collaborative. All rights reserved.
+# This file is part of the BlueButton.py project.
+# Use of this source code is governed by the license found in the LICENSE file.
+###############################################################################
+
 '''/*
  * Parse_r for the C32 document section
  */
