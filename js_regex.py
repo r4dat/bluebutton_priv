fc6cef064f052aee7f0db5275b2b8881d06c5e5d
Rob R <RRutherford@khconline.org>
2015-07-07 15:49:27 -0500

    Update regex replace piece.

--- js_regex.py
+++ js_regex.py
@@ -5,17 +5,24 @@ Created on Fri Jun 26 12:19:08 2015
 @author: RobR
 """
 import re
-out=open(r'C:\Users\User\Documents\bluebutton_priv\bluebutton\parsers\_c32\enc_out.py','w')
+out=open(r'C:\Users\robrutherford\Documents\bluebutton_priv\bluebutton\parsers\_c32\imm_out.py','w')
 
-with open(r'C:\Users\User\Documents\bluebutton_priv\bluebutton\parsers\_c32\encounters.py') as f:
+with open(r'C:\Users\robrutherford\Documents\bluebutton_priv\bluebutton\parsers\_c32\immunizations.py') as f:
     for line in f:
         dat = line
-        # Fix opening comment.
+        # Fix C style comments.
         dat = re.sub('//','#',dat)
-        
+
+        # Fix C style long comment.
+        dat = re.sub('\/\*','\'\'\'',dat)
+        dat = re.sub('\*\/','\'\'\'',dat)
+
         # Def function
         dat = re.sub('.*\.([a-z]*).*(c32).*','def \\1(\\2):',dat)
-        
+
+        # Fix initial boolean syntax.
+        dat = re.sub('if \(\!([a-z]*)\) \{','if not \\1:',dat)
+
         #Remove '  var '
         dat = re.sub('  var ','    ',dat)
         
@@ -23,11 +30,14 @@ with open(r'C:\Users\User\Documents\bluebutton_priv\bluebutton\parsers\_c32\enco
         dat = re.sub('(parse)([A-Z][a-z]*)','\\1_\\2',dat).lower()
         
         #Replace Core.stripWhitespace with pythonized shim.
-        dat = re.sub('(Core\.strip)(W[a-z]*','\\1_\\2',dat).lower()
+        dat = re.sub('(Core\.strip)(W[a-z]*)','\\1_\\2',dat).lower()
         
         dat = re.sub('documentation_of_list = \[\]','documentation_of_list = wrappers.ListWrapper()',dat)
         
         dat = re.sub(';','',dat)
+
+        # Replace JS funcs with python shim funcs.
+        dat = re.sub('el.boolattr','el.bool_attr',dat)
         
         #dat = re.sub('for \(var i.*performers.*\{','for el in performers:',dat)
         out.write(dat)
